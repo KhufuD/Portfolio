c46f5dd355b4c25886839bd09890e4af9be739d2
Khufu <khufudev@gmail.com>
2022-04-15 01:32:42 +0500

    created Banner

--- js/index.js
+++ js/index.js
@@ -1,41 +1,10 @@
-// Moving the cards in and out
+window.addEventListener('scroll', () => {
+    let movingtext = document.getElementById("moving-text");
+    let scrolled = window.pageYOffset;
+    let left = scrolled * 3;
+    movingtext.style.transform = `translate3d(-${left}px, 0px, 0px)`;
 
-
-
-// const aboutClass = document.getElementsByClassName("about");
-// const skillsClass = document.getElementsByClassName("skills");
-// const whyMeClass = document.getElementsByClassName("why-me");
-// const portfolioClass = document.getElementsByClassName("portfolio");
-// const hireMeClass = document.getElementsByClassName("hire-me");
-// const newsLetterClass = document.getElementsByClassName("news-letter");
-
-
-// const skillsId = document.getElementById("skills");
-// const whyMeId = document.getElementById("why-me");
-// const portfolioId = document.getElementById("portfolio");
-// const hireMeId = document.getElementById("hire-me");
-// const newsLetterId = document.getElementById("news-letter");
-
-// Moving the card in
-
-// aboutId.addEventListener("click", slide());
-
-const aboutId = document.getElementById("about");
-
-function aboutSlideIn() {
-    let element = document.getElementById("about-card");
-    element.classList.add("about-in");
-    element.classList.remove("card-out");
-
-}
-
-// Moving the card out
-
-const arrow = document.getElementById("card-btn");
-
-function aboutSlideOut() {
-    let element = document.getElementById("about-card");
-    element.classList.add("card-out");
-    element.classList.remove("about-in");
-
-}
+    let reverseMove = document.getElementById("reverse-movement");
+    let right = left;
+    reverseMove.style.transform = `translate3d(-${right}px, 0px, 0px)`;
+})
