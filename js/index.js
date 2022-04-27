1b2d71355e800388911544079478039c6b29eb8d
khufu <khufudev@gmail.com>
2022-04-27 12:56:59 -0700

    v1 complete

--- js/index.js
+++ js/index.js
@@ -120,3 +120,19 @@ function sendEmail(){
       message => alert(message)
     );
 }
+
+document.addEventListener(
+    "scroll",
+    function() {
+      var scrollTop =
+        document.documentElement["scrollTop"] || document.body["scrollTop"];
+      var scrollBottom =
+        (document.documentElement["scrollHeight"] ||
+          document.body["scrollHeight"]) - document.documentElement.clientHeight;
+      scrollPercent = scrollTop / scrollBottom * 100 + "%";
+      document
+        .getElementById("_progress")
+        .style.setProperty("--scroll", scrollPercent);
+    },
+    { passive: true }
+  );
