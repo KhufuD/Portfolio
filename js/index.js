9d7e3b22b61aedbb96311a23af5eb852cfd1dfac
khufu <khufudev@gmail.com>
2022-04-28 14:04:28 -0700

    responsive menu created

--- js/index.js
+++ js/index.js
@@ -136,3 +136,19 @@ document.addEventListener(
     },
     { passive: true }
   );
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
